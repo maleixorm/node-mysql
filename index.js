3d04c21c6a592a3b5e21dac4d9e5a4e5b125b71d
Marcos Aleixo <67377101+maleixorm@users.noreply.github.com>
2025-09-14 00:48:01 -0300

    Conectando ao banco de dados criado para o projeto

--- index.js
+++ index.js
@@ -1,13 +1,29 @@
-import e from "express";
-import { ExpressHandlebars } from "express-handlebars";
-import { MySQL } from "mysql";
+const express = require('express')
+const epxhbs = require('express-handlebars')
+const mysql = require('mysql2')
 
-const app = e();
-app.engine('handlebars', ExpressHandlebars())
+const app = express();
+app.engine('handlebars', epxhbs.engine())
 app.set('view engine', 'handlebars')
+app.set('views', './views');
 
-app.use(e.static('public'))
+app.use(express.static('public'))
 
 app.get('/', (req, res) => {
     res.render('home')
+})
+
+const conn = mysql.createConnection({
+    host: 'localhost',
+    user: 'php',
+    password: '123456',
+    database: 'node_mysql'
+})
+
+conn.connect(function(err) {
+    if (err) {
+        console.log(err)
+    }
+    console.log('Conectado ao Banco de dados!')
+    app.listen(3000)
 })
